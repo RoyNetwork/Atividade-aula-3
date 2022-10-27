8408054119d948423d731755735ee955c281a4b2
Alan (Roy Network) <106408391+RoyNetwork@users.noreply.github.com>
2022-10-27 17:33:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//interruptor  ( placar )  {
+    //placar de caixa : vaxco >  memengo
+       //consola . log ( 'vaxco ganhou' )
+      // quebrar ;
+   //case  placar  =  memengo  >  vaxco :
+      // consola . log ( 'memengo ganhou' )
+      // quebrar ;
+  // padrão :
+           //consola . log ( 'ninguém ganha, que pena...' )
+          // quebrar ;
+
+
+           var signo = prompt('Digite seu signo: ')
+           switch (signo) {
+            case 'Aries':
+                console.log('de 21 março a 20 abril')
+                break;
+            case 'Touro':
+                console.log('de 21 abril a 20 maio')
+                case 'Gemeos':
+                console.log('de 21 maio a 20 junho')
+                break;
+            case 'Cancer':
+                console.log('de 23 julho a 22 agosto')
+                case 'Leao':
+                console.log('de 23 julho a 22 agosto')
+                break;
+            case 'Virgem':
+                console.log('de 23 agosto a 22 setembro')
+            default:  
+                console.log('Signo não registrado')
+                break;
+           }
+           //Aries - de 21 março a 20 abril
+           //Touro -  de 21 abril a 20 maio
+           //Gemeos - de 21 maio a 20 junho
+           //Cancer - de 21 junho a 22 julho
+          // Leao - de 23 julho a 22 agosto
+           //Virgem - de 23 agosto a 22 setembro
+           //Caso não tenha o signo - Signo não registrado
